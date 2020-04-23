9c6b5ed17afbc48d5dbae6d4ae6489aab8caacb1
ML3636 <markus.lam22@ycdsbk12.ca>
2020-04-23 19:37:46 -0400

    first commit

---
+++ Even or odd.py
@@ -0,0 +1,5 @@
+N = int(input("Please print the number"))
+if N % 2 ==0:
+    print ('Even')
+if number % 2 !=0:
+    print ('Odd')
---
+++ Months.py
@@ -0,0 +1,19 @@
+Month = int(input("Please print the month with a capital first letter and the rest lowercase."))
+
+if Month == "January"
+or Month == "March"
+or Month == "May"
+or Month == "July"
+or Month == "August"
+or Month == "October"
+or Month == "December"
+print ("31 days")
+
+if Month == "Febuary"
+print ("28 or 29 days")
+
+if Month == "April"
+or Month == "June"
+or Month == "September"
+or Month == "November"
+print ("30 days")
