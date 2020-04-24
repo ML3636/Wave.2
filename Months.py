b21e259da52c153db2d76861e53feb74074b42c2
ML3636 <markus.lam22@ycdsbk12.ca>
2020-04-24 14:54:45 -0400

    first commit

--- Months.py
+++ Months.py
@@ -1,10 +1,9 @@
-month = int(input("Please print the month with a capital first letter and the rest lowercase."))
+month = str
+month = input("Please print the month with a capital first letter and the rest lowercase.")
 
-if month == "January" or month == "March" or month == "May" or month == "July" or month == "August" or month == "October" or month == "December"
+     or month == "March" or month == "May" or month == "July" or month == "August" or month == "October" or month == "December"
     print ("31 days")
-
 if month == "Febuary"
     print ("28 or 29 days")
-
 if month == "April" or month == "June" or month == "September"or month == "November"
     print ("30 days")
---
+++ colorofsquare.py
@@ -0,0 +1,27 @@
+L = str(input("Please print letter position on the board (hit enter after letter): ")) 
+N = int(input("Please print a number position on the board: ")) 
+
+if (L == "a"): 
+if (N % 2 != 0): print("black") 
+else: print("white") 
+if (L == "c"): 
+if (N % 2 != 0): print("black") 
+else: print("white") 
+if (L == "e"): 
+if (N % 2 != 0): print("black") 
+else: print("white") 
+if (L == "g"): 
+if (N % 2 != 0): print("black") 
+else: print("white")
+if (L == "b"): 
+if (N % 2 != 0): print("white") 
+else: print("black") 
+if (L == "d"): 
+if (N % 2 != 0): print("white") 
+else: print("black") 
+if (L == "f"): 
+if (N % 2 != 0): print("white") 
+else: print("black") 
+if (L == "h"): 
+if (N % 2 != 0): print("white") 
+else: print("black")
